1d6fa34edd28393ce6366c4286ae82b038c628e8
lasgedu <c.unekwe@alustudent.com>
2024-03-01 00:26:45 -0800

    All

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -5,9 +5,9 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    url = "https://www.reddit.com/r/{stag12}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/lasgedu)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
